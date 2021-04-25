e0b06b2feacd03d691696768af8986d573a4bb2b
amainkar <atharva.mainkar@gmail.com>
2021-04-25 17:32:15 -0500

    Deleted stuff

--- Online Shopping System/src/RunClassesMain.java
+++
@@ -1,6 +0,0 @@
-public class RunClassesMain {
-    public static void main(String Args[]){
-        System.out.println("Hello world");
-        //Use this class to run and execute all other classes
-    }
-}
